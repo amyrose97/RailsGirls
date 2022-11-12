feb84cd6a6f6aa6c646b390acb5358606069e934
Amy <amyhassall@hotmail.co.uk>
2022-11-12 16:40:38 +0000

    Move config host to right file

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ module Railgirls
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.hosts << "railsgirls-production-amy-rose-x-000.up.railway.app"
+
 
     # Configuration for the application, engines, and railties goes here.
     #
